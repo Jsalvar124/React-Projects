223945d8691fcf023675603e4cc1b0b6f4e69453
Jsalvar124 <jsalvar124@gmail.com>
2023-03-08 18:22:04 -0500

    fixed file with standard

--- 01-twitter-card/src/App.jsx
+++ 01-twitter-card/src/App.jsx
@@ -1,65 +1,65 @@
-import './App.css';
-import { TwitterFollowCard } from './TwitterFollowCard.jsx';
-import { useState } from "react";
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+import { useState } from 'react'
 
-export function App() {
+export function App () {
+  const [showMore, setShowMore] = useState(false)
 
-    const [showMore, setShowMore] = useState(false);
+  const objectUser = {
+    initialIsFollowing: false,
+    name: 'Random User',
+    userName: 'randUs'
+  }
 
-    const objectUser = {
-        initialIsFollowing: false,
-        name: "Random User",
-        userName: "randUs"
+  const users = [ /* testing map to render multiple components */
+    {
+      initialIsFollowing: true,
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán'
+    },
+    {
+      initialIsFollowing: false,
+      userName: 'PacoHdezs',
+      name: 'Pablo Hernandez'
+    },
+    { // Empty user to test default props
+    },
+    {
+      initialIsFollowing: true,
+      userName: 'facebook',
+      name: 'Facebook Meta'
     }
+  ]
 
-    const users = [  /* testing map to render multiple components */
-        {
-            initialIsFollowing: true,
-            userName: "midudev",
-            name: "Miguel Ángel Durán"
-        },
-        {
-            initialIsFollowing: false,
-            userName: "PacoHdezs",
-            name: "Pablo Hernandez"
-        },
-        {  // Empty user to test default props
-        },
-        {
-            initialIsFollowing: true,
-            userName: "facebook",
-            name: "Facebook Meta"
-        }
-    ]
+  const showMoreOnClick = () => {
+    setShowMore(!showMore)
+  }
 
-    const showMoreOnClick = () => {
-        setShowMore(!showMore)
-    }
-
-    return (
-        <section className='App'>
-            <span className='tw-articleTitle'>A quién seguir</span>
-            <TwitterFollowCard initialIsFollowing={false} userName="jsalvar124"> {/* testing Children */}
-                Julián Salvá Ramírez
-            </TwitterFollowCard>
-            <TwitterFollowCard initialIsFollowing={true} userName="basujindal" name="Stable-difussion" />
-            <TwitterFollowCard userName="lucidrains" name="DALEE-2" />
-            <TwitterFollowCard {...objectUser} /> {/*testing props by object and spread operator */}
-            {
+  return (
+    <section className='App'>
+      <span className='tw-articleTitle'>A quién seguir</span>
+      <TwitterFollowCard initialIsFollowing={false} userName='jsalvar124'> {/* testing Children */}
+        Julián Salvá Ramírez
+      </TwitterFollowCard>
+      <TwitterFollowCard initialIsFollowing userName='basujindal' name='Stable-difussion' />
+      <TwitterFollowCard userName='lucidrains' name='DALEE-2' />
+      <TwitterFollowCard {...objectUser} /> {/* testing props by object and spread operator */}
+      {
                 users.map((user, id) => {
-                    const { userName, name, initialIsFollowing } = user
+                  const { userName, name, initialIsFollowing } = user
 
-                    if (showMore) {
-                        return (
-                            <TwitterFollowCard userName={userName} name={name} initialIsFollowing={initialIsFollowing} key={userName + id} />
-                        )
-                    }
+                  if (showMore) {
+                    return (
+                      <TwitterFollowCard userName={userName} name={name} initialIsFollowing={initialIsFollowing} key={userName + id} />
+                    )
+                  } else {
+                    return ('')
+                  }
                 })
             }
 
+      <a onClick={showMoreOnClick} className='tw-showMore'>Mostrar más</a>
 
-            <a onClick={showMoreOnClick} className='tw-showMore'>Mostrar más</a>
-
-        </section>
-    )
+    </section>
+  )
 }
