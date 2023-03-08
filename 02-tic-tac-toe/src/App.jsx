758adea84d7835a557c5d501b304d8dfab906428
Jsalvar124 <jsalvar124@gmail.com>
2023-03-08 18:20:33 -0500

    triki game and linter config

---
+++ 02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinner } from './logic/board.js'
+
+function App () {
+  //  se crea el tablero, como un estado.
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  }
+
+  )
+
+  //  turnos, como un estado, puede tomar el valor de 'x' u 'o'
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X
+  })
+  const [winner, setWinner] = useState(null) // null, no hay ganador, false empate.
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    //  revisar si queda algún valor null dentro del array de celdas.
+    return newBoard.every((square) => square !== null)
+  }
+
+  const updateBoard = (index) => {
+    // si winner es NOT null, quiere decir que el juego acabó y no deja seguir cambiando el tablero.
+    if (board[index] || winner) return // si el valor dentro del square, es null, pues no hace nada, eso permite evitar que se modifiquen celdas ya llenas.
+
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn // x u o
+    console.log(newBoard)
+    setBoard(newBoard)
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', JSON.stringify(newTurn))
+    // revisar si alguien ha ganado
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+      // alert(`felicidades, ha ganado ${turn}`)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+  return (
+    <main className='board'>
+      <h1>Triki</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'El ganador es'
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+
+          </section>
+        )
+      }
+    </main>
+  )
+}
+
+export default App
---
+++ 02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,15 @@
+import { WINNER_COMBOS } from "../constants"
+
+
+export const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+  }
