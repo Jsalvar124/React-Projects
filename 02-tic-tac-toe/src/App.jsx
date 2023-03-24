a8037f5535c2acc2113ec778436c75219d8cd1e5
Jsalvar124 <jsalvar124@gmail.com>
2023-03-23 21:31:00 -0500

    se añade mouse follower y triqui con transparencia para ver quien ganó

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -25,6 +25,9 @@ function App () {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const checkEndGame = (newBoard) => {
